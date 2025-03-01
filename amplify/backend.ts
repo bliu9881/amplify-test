b8b8b853d05c8f77fedc52b0a8f5b45a078647b3
brian Liu <bliu9881@gmail.com>
2025-02-28 16:20:35 -0800

    jdjdj

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,24 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { storage } from "./storage/resource";
+import { clickStreamFunction } from "./functions/clickdata/resource";
+import { FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  storage,
+  clickStreamFunction,
+});
+
+const clickstreamfunctionUrlObj =
+  backend.clickStreamFunction.resources.lambda.addFunctionUrl({
+    authType: FunctionUrlAuthType.NONE,
+  });
+
+backend.addOutput({
+  custom: {
+    clickstreamfunctionUrl: clickstreamfunctionUrlObj.url,
+  },
 });
